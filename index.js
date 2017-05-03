e0de14b8d3466c4b2932ee9a6505684fb7c3f873
Kyle Roden <Kyle Roden>
2017-05-02 20:50:21 -0700

    adding postgres

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var faces = require('cool-ascii-faces');
+var pg = require('pg');
 var app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,6 +19,21 @@ app.get('/', function(request, response) {
 app.get('/cool', function(request, response) {
   response.send(faces());
 });
+
+app.get('/db', function(request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if(err) {
+        console.error(err);
+        response.send("ERROR ", err);
+        }
+      else {
+        response.render('pages/db', {results: result.rows} );
+        }
+      });
+    });
+  });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
