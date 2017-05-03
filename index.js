142a6eb85d6ce7c622c82b8bdef2f9b9ec00edf5
Kyle Roden <Kyle Roden>
2017-05-02 17:00:41 -0700

    demo faces

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var faces = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,8 +14,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/cool', function(request, response) {
+  response.send(faces());
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
