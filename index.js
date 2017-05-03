e492948feb0172e5d2b6e8e63c42ca5b95f5d65d
Kyle Roden <Kyle Roden>
2017-05-03 07:57:33 -0700

    local db url change

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/cool', function(request, response) {
 });
 
 app.get('/db', function(request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pg.connect(process.env.DATABASE_URL || 'postgres://localhost:5000', function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if(err) {
