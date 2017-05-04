cb5522b31dff79fc266359d5278d72b9333c990b
kyleroden <kyleroden77@gmail.com>
2017-05-04 15:33:15 -0700

    writing to file sucessful

--- index.js
+++ index.js
@@ -34,8 +34,22 @@ app.get('/bikes', function(request, response) {
         console.log("error: ", err);
         return response.status(500).send("there was an error in calling the bikes api");
       }
+
       //console.log(res);
       const bike_list = res.body.data.bikes;
+      //get the current date, then write to the json file: date: bounty count
+      const current_date = new Date();
+      const bounty_obj = {};
+      bounty_obj.date = current_date;
+      bounty_obj.count = bike_list.length;
+
+      console.log(bounty_obj);
+      fs.writeFile('./bounties.json', JSON.stringify(bounty_obj), function(err) {
+        if(err) {
+          console.log("error: ", err);
+        }
+        console.log("server wrote bounty_obj to file");
+      });
       //response.status(200).send(bike_list);
       response.render('pages/biketown', {results: bike_list} );
     });
