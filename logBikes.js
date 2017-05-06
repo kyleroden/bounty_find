47dd5add41ab2988d5a31d488e1a782ad77db0f1
Rochele Roden <rocheleroden@Rocheles-Air.local>
2017-05-06 11:37:35 -0700

    add console log on logBikes.js

--- logBikes.js
+++ logBikes.js
@@ -10,7 +10,8 @@ function logBikes(){
         const bounty_obj = {};
         bounty_obj.date = current_date;
         bounty_obj.count = bike_total;
-
+        //check to make sure this is working from heroku server
+        console.log("date variable from server: ", current_date);
         console.log(bounty_obj);
         fs.readFile('./bounties.json', function(error, data) {
             var json = JSON.parse(data);
