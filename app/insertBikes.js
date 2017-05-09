8e4811305459a1a23ca6993cefb2e419b28ba6ee
Kyle Roden <kyleroden77@gmail.com>
2017-05-08 17:45:09 -0700

    aniyo

--- app/insertBikes.js
+++ app/insertBikes.js
@@ -9,38 +9,38 @@ var pg = require('pg');
 var request_super = require('superagent');
 console.log('ran outside of logbikes function');
 function logBikes() {
-    request_super.get('http://biketownpdx.socialbicycles.com/opendata/free_bike_status.json')
-        .end(function(err, response) {
-            let current_bounty_inventory = response.data.bikes;
-            let bike_total = current_bounty_bikes.length;
-            const current_date = new Date();
 
-            //check to make sure this is working from heroku server
-            console.log("date variable from server: ", current_date);
-            console.log("success");
-            console.log(bounty_obj);
-
-            //now put the data into the pg database 'bounties'
-
-            // pg.connect(process.env.DATABASE_URL || 'bikebounties://localhost:5000', function(err, client, done) {
-            //     client.query('INSERT INTO bounties values(' + "'" + current_date + "'" + ', ' + "'" + bike_total + "'" + ')',
-            //         function(err, result) {
-            //             done();
-            //             if (err) {
-            //                 console.error(err);
-            //                 response.send("ERROR ", err);
-            //             } else {
-            //                 //
-            //                 // response.render('pages/db', {
-            //                 //     results: result.rows
-            //             }
-            //             client.end();
-            //           });
-            // });
-        });
  }//end api call
 
 logBikes();
+request_super.get('http://biketownpdx.socialbicycles.com/opendata/free_bike_status.json')
+    .end(function(err, response) {
+        let current_bounty_inventory = response.data.bikes;
+        let bike_total = current_bounty_bikes.length;
+        const current_date = new Date();
 
+        //check to make sure this is working from heroku server
+        console.log("date variable from server: ", current_date);
+        console.log("success");
+        console.log(bounty_obj);
+
+        //now put the data into the pg database 'bounties'
+
+        // pg.connect(process.env.DATABASE_URL || 'bikebounties://localhost:5000', function(err, client, done) {
+        //     client.query('INSERT INTO bounties values(' + "'" + current_date + "'" + ', ' + "'" + bike_total + "'" + ')',
+        //         function(err, result) {
+        //             done();
+        //             if (err) {
+        //                 console.error(err);
+        //                 response.send("ERROR ", err);
+        //             } else {
+        //                 //
+        //                 // response.render('pages/db', {
+        //                 //     results: result.rows
+        //             }
+        //             client.end();
+        //           });
+        // });
+    });
 //needed in order to use this script as a heroku worker
 process.exit();
